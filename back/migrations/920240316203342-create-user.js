d63d5ec40e105447c313132ce743703a451684cd
Marina Laguna Valdepeñas <marinalaguna2004@gmail.com>
2024-03-17 01:13:17 +0100

    create migrations

---
+++ back/migrations/920240316203342-create-user.js
@@ -0,0 +1,69 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable(process.env.TABLE_USERS, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        onDelete: 'CASCADE'
+      },
+      firsName: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique:true
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull:true
+      },
+      photo_profile: {
+        defaultValue: 1,
+        type: Sequelize.INTEGER,
+        references:{
+          model: {
+            tableName: process.env.TABLE_ASSETS
+          },
+          key: 'id'
+        },
+      },
+      born_date: {
+        type: Sequelize.DATE,
+        allowNull:true
+      },
+      domicile: {
+        type: Sequelize.STRING,
+        allowNull:true
+      },
+      phone_number: {
+        type: Sequelize.STRING,
+        allowNull:true
+      },
+      active: {
+        type: Sequelize.BOOLEAN,
+        allowNull:false,
+        defaultValue: 1,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable(process.env.TABLE_USERS);
+  }
+};
