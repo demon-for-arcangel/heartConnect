c5114caeaadd3ef3bd770842f8cb64e1206cd36e
Marina Laguna Valdepeñas <marinalaguna2004@gmail.com>
2024-03-17 01:51:54 +0100

    create "abilities.js" for to control access with token

---
+++ back/middlewares/abilities.js
@@ -0,0 +1,83 @@
+const jwt = require("jsonwebtoken");
+const Conexion = require("../database/users/UserConnection");
+
+const statusUser = (req, res, next) => {
+    const conx = new Conexion();
+    conx
+      .getUserByEmail(req.body.email)
+      .then((msg) => {
+        if (msg.active == 1) {
+          next();
+        } else {
+          res.status(400).json({ msg: "account desactivated" });
+        }
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(400).json({ msg: "user not found", error: err });
+    });
+};
+
+const checkToken = (req, res, next) => {
+    const token = req.header("x-token");
+  
+    if (!token) {
+      return res.status(401).json({ msg: "No hay token en la petición." });
+    }
+  
+    try {
+      const { uid, roles } = jwt.verify(token, process.env.SECRETORPRIVATEKEY);
+      req.userId = uid;
+      req.uroles = roles;
+      console.log(uid);
+      console.log(token);
+      next();
+    } catch (error) {
+      console.log(error);
+      res.status(401).json({ msg: "Token no valido" });
+    }
+};
+
+const tokenCanAdmin = (req, res, next) => {
+    let roles = req.uroles;
+    let i = 0;
+    let check = true;
+    while (i < roles.length && check) {
+        if (roles[i] == process.env.ID_ROL_ADMIN) {
+        check = false;
+        }
+        i++;
+    }
+
+    if (!check) {
+        next();
+    } else {
+        res.status(400).json({ msg: "Token sin permisos" });
+    }
+};
+
+const tokenCanUser = (req, res, next) => {
+    let roles = req.uroles;
+    let i = 0;
+    let check = true;
+    while (i < roles.length && check) {
+      if (roles[i] == process.env.ID_ROL_USER || 
+        roles[i] == process.env.ID_ROL_ADMIN) {
+        check = false;
+      }
+      i++;
+    }
+  
+    if (!check) {
+      next();
+    } else {
+      res.status(400).json({ msg: "Token sin permisos" });
+    }
+};
+
+module.exports = {
+    statusUser,
+    checkToken,
+    tokenCanAdmin,
+    tokenCanUser
+}
