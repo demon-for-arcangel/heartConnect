2bd3f33e203894b0d5deff2d6afbfb8390e12b62
Marina Laguna Valdepeñas <123579383+demon-for-arcangel@users.noreply.github.com>
2024-03-18 15:25:51 +0100

    update

---
+++ back/factories/userRolFactory.js
@@ -0,0 +1,10 @@
+const { faker } = require('@faker-js/faker');
+const Conexion = require("../database/users/UserConnection");
+
+const userRolFactory = async (num_gen) => {
+
+}
+
+module.exports = { 
+    userRolFactory
+};
