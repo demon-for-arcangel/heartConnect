704a895c94dfd0376cb3d713ef34bc7960040e3c
Marina Laguna Valdepeñas <marinalaguna2004@gmail.com>
2024-03-19 10:20:12 +0100

    create rol seeder and modification in the database model

--- back/seeders/20240318120004-rols.js
+++ back/seeders/20240318120004-rols.js
@@ -3,23 +3,23 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.bulkInsert(process.env.TABLE_ROLS, [
+      {
+        id: process.env.ID_ROL_ADMIN,
+        name: "administrador",
+        desc: "Administrador de toda la aplicación."
+      },
+      {
+        id: process.env.ID_ROL_USER,
+        name: "usuario",
+        desc: "Usuario normal que disfruta de la aplicación."
+      }
+    ],
+    {}
+    );
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete(process.env.TABLE_ROLS, null, {});
   }
 };
