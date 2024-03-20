e29fadfd2aa4d456e669e5f86781250b04ef0a2e
Marina Laguna Valdepeñas <marinalaguna2004@gmail.com>
2024-03-20 14:58:31 +0100

    created assets seeder

--- back/seeders/20240318120038-assets.js
+++ back/seeders/20240318120038-assets.js
@@ -3,23 +3,13 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    let arrPhotos = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'];
+
+    let photos_profile = await assetsFactory(arrPhotos)
+    await queryInterface.bulkInsert(process.env.TABLE_ASSETS, photos_profile);
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete(process.env.TABLE_ASSETS, null, {})
   }
 };
