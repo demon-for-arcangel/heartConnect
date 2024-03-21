970255cf2e7e33442543aec1535726c37d3db959
Marina Laguna Valdepeñas <marinalaguna2004@gmail.com>
2024-03-21 11:17:57 +0100

    removing unused dependencies

--- back/app/server.js
+++ back/app/server.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require('dotenv').config()
-const mongoose = require("mongoose");
 const { socketController } = require("../controllers/services/socketController");
-mongoose.set("strictQuery", false);
 const fileUpload = require('express-fileupload');
 
 class Server {
@@ -25,39 +23,8 @@ class Server {
         }
       });
   
-      this.conectarMongoose();
       this.routes();
       this.sockets()
-    }
-  
-    conectarMongoose() {
-      mongoose.connect(
-        "mongodb://" +
-          process.env.DB_MONGO_URL +
-          ":" +
-          process.env.DB_MONGO_PORT +
-          "/" +
-          process.env.DB_MONGO_DATABASE
-          /**
-           * En la consola salta un warning advirtiendo de que 
-           * esos atributos están deprecated. 
-           * 
-           */
-        //   ,
-        // {
-        //   useNewUrlParser: true,
-        //   useUnifiedTopology: true,
-        // }
-      );
-  
-      this.db = mongoose.connection;
-      this.db.on(
-        "error",
-        console.error.bind(console, "Error de conexión a MongoDB:")
-      );
-      this.db.once("open", () => {
-        //console.log("Conexión exitosa a MongoDB");
-      });
     }
   
     middlewares() {
@@ -75,14 +42,7 @@ class Server {
     }
   
     routes() {
-/*       this.app.use(this.apiFiles, require("../routes/services/uploadsRoutes"));
- */      this.app.use(this.userRoutePath, require("../routes/users/userRoutes"));
-      /* this.app.use(this.userRoutePath, require("../routes/users/alergiasRoutes")); */
-/*       this.app.use(this.userRoutePath, require("../routes/news/newsRoutes"));
- */      /* this.app.use(this.userRoutePath, require("../routes/landing/clubRoutes")); */
-      /* this.app.use(this.userRoutePath, require("../routes/landing/contactRoutes"));
-      this.app.use(this.userRoutePath, require("../routes/assetsRoutes"));
-      this.app.use(this.apiMail, require("../routes/services/mailRoutes.js")); */
+      this.app.use(this.userRoutePath, require("../routes/users/userRoutes"));
     }
   
     sockets() {
@@ -96,8 +56,8 @@ class Server {
   
       this.serverWebSocket.listen(process.env.WEBSOCKETPORT, () => {
         //console.log(`Servidor de WebSockets escuchando en: ${process.env.WEBSOCKETPORT}`);
-      });
+      }); 
     }
-  }
+ }
   
-  module.exports = Server;
+ module.exports = Server;
