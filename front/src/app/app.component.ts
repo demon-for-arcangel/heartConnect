63c3b840758af983c0cc998007653b49c04829ea
Marina Laguna Valdepeñas <marinalaguna2004@gmail.com>
2024-03-22 15:04:46 +0100

    update

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ConfirmationService, PrimeNGConfig } from 'primeng/api';
+import { MessageService } from 'primeng/api';
+import { WebsocketsService } from './services/websockets.service';
+import { ToastModule } from 'primeng/toast';
+import { environment } from './environments/environment.development';
+import { io } from 'socket.io-client';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ToastModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [ConfirmationService, MessageService]
 })
 export class AppComponent {
-  title = 'front';
+  title = 'HeartConnect';
+  isLogged = false;
+
+  private socket: any; 
+
+  constructor(
+    private primengConfig: PrimeNGConfig, 
+    private messageService: MessageService, 
+    private websocket: WebsocketsService
+  ){}
+
+  ngOnInit() {
+    this.socket = io(environment.websocket)
+  }
 }
