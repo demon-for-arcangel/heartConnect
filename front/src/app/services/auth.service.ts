d3f01b8fc241561ff9648c6aa248b925bed4d460
Marina Laguna <marinalaguna2004@gmail.com>
2024-03-23 18:11:00 +0100

    create auth service

--- front/src/app/services/auth.service.ts
+++ front/src/app/services/auth.service.ts
@@ -1,9 +1,52 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment.development';
+import { Observable } from 'rxjs';
+import { User } from '../interfaces/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  url = environment.baseUrl+environment.myProfile
+
+  getUserByToken(): Observable<User | undefined>{
+    return this.http.get(this.url, {withCredentials: true})
+  }
+
+  isLoggedIn(): boolean{
+    const user = this.getCurrentUser();
+    return user !== null;
+  }
+
+  getCurrentUser(): any {
+    const user = localStorage.getItem('user');
+    return user ? JSON.parse(user) : null;
+  }
+
+  logout(): void {
+    localStorage.removeItem('user');
+  }
+
+  getRolesOfToken(): any {
+    try {
+      let token = JSON.parse(localStorage.getItem('user') as string).token.split('.')[1];
+      const rolesOfToken = JSON.parse(atob(token)).roles;
+      return rolesOfToken;
+    } catch (error){
+      return null;
+    }
+  }
+
+  getIdOfToken(): any {
+    try {
+      let token = JSON.parse(localStorage.getItem('user') as string).token.split('.')[1]
+      const idOfToken = JSON.parse(atob(token)).uid;
+      return idOfToken;
+    } catch (error){
+      return null;
+    }
+  }
 }
