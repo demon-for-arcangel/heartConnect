88a8bb562566ed9fde37a81551fa5f3c5073ef43
Marina Laguna <marinalaguna2004@gmail.com>
2024-03-24 21:57:27 +0100

    update register in the client

--- front/src/app/components/register/register.component.ts
+++ front/src/app/components/register/register.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import mbxGeocoding from '@mapbox/mapbox-sdk/services/geocoding';
 @Component({
   selector: 'app-register',
   standalone: true,
@@ -7,6 +7,25 @@ import { Component } from '@angular/core';
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  @ViewChild('domicile') domicileInput!: ElementRef;
+  ngOnInit() {
+    const geocodingClient = mbxGeocoding({ accessToken: 'YOUR_MAPBOX_ACCESS_TOKEN' });
 
+    this.domicileInput.nativeElement.addEventListener('input', (event: Event) => {
+      const target = event.target as HTMLInputElement;
+      const searchText = target.value;
+      geocodingClient.forwardGeocode({
+        query: searchText,
+        autocomplete: true,
+        limit: 5
+      })
+      .send()
+      .then((response: any) => {
+        const suggestions = response.body.features;
+        // Aquí puedes procesar las sugerencias y mostrarlas en el contenedor de sugerencias
+        console.log(suggestions);
+      });
+    });
+ }
 }
---
+++ front/src/types/mapbox-sdk.d.ts
@@ -0,0 +1,2 @@
+// mapbox-sdk.d.ts
+declare module '@mapbox/mapbox-sdk/services/geocoding';
