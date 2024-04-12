34dae5a0d9f9f6e4c711914f6ca6b6c225324df9
Marina Laguna Valdepeñas <marinalaguna2004@gmail.com>
2024-04-12 16:25:04 +0200

    control to show the administrator menu and the user menu

--- front/src/app/components/dashboard/dashboard.component.ts
+++ front/src/app/components/dashboard/dashboard.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { MenuAdminComponent } from '../shared/menu-admin/menu-admin.component';
+import { MenuComponent } from '../shared/menu/menu.component';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [MenuAdminComponent],
+  imports: [MenuAdminComponent, MenuComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  isAdmin: boolean = false;
 
+  constructor(private authService: AuthService) {}
+  
+  ngOnInit():void{
+    this.isAdmin = this.authService.isAdmin();
+  }
 }
