fa1d73cc0d5b53911515e2b1687258042ae3fad4
Marina Laguna Valdepeñas <marinalaguna2004@gmail.com>
2024-04-12 19:43:38 +0200

    update

--- front/src/app/components/information/information.component.ts
+++ front/src/app/components/information/information.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MenuHomeComponent } from '../shared/menu-home/menu-home.component';
+import { FooterComponent } from '../shared/footer/footer.component';
 
 @Component({
   selector: 'app-information',
   standalone: true,
-  imports: [MenuHomeComponent],
+  imports: [MenuHomeComponent, FooterComponent],
   templateUrl: './information.component.html',
   styleUrl: './information.component.css'
 })
