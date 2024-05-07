64cf2a97b5280b4fad08206ce6926a2cedf7800d
Marina Laguna Valdepeñas <marinalaguna2004@gmail.com>
2024-05-07 23:48:42 +0200

    update seeders

--- back/seeders/20240318120256-user_events.js
+++ back/seeders/20240318120256-user_events.js
@@ -1,26 +1,24 @@
 'use strict';
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(process.env.TABLE_USER_EVENTS, [
+      {
+        id_user: 2,
+        id_events: 1, 
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id_user: 3, 
+        id_events: 2, 
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+    ], {});
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete(process.env.TABLE_USER_EVENTS, null, {});
   }
 };
-//Hacer
