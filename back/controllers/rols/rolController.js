c702337cd6c283ec42f4ab8b9197b2ab3deb4258
Marina Laguna Valdepeñas <marinalaguna2004@gmail.com>
2024-05-07 16:05:49 +0200

    create function showRols in controller and routes

--- back/controllers/rols/rolController.js
+++ back/controllers/rols/rolController.js
@@ -1,1 +1,19 @@
-//hacer
+const { response, request } = require("express");
+const Conexion = require("../../database/users/UserConnection");
+const models = require('../../models');
+
+const conx = new Conexion();
+
+const index = async (req, res) => {
+    try{
+        const rols = await conx.indexRols();
+        res.status(200).json(rols);
+    }catch (error){
+        console.error('Error al obtener los roles', error);
+        res.status(500).json({ msg: "Error"});
+    }
+}
+
+module.exports = {
+    index, 
+};
--- back/routes/rols/rolRoutes.js
+++ back/routes/rols/rolRoutes.js
@@ -1,1 +1,8 @@
-//hacer
+const {Router } = require('express');
+const controlador = require('../../controllers/rols/rolController.js');
+const { check } = require('express-validator');
+const { validateFilds, checkDiferenceAsign } = require('../../middlewares/validators.js');
+const { statusUser, tokenCanAdmin, tokenCanUserAuth, checkToken, tokenCanSocio } = require('../../middlewares/abilities');
+const router = Router();
+
+router.get('/users/', /*[checkToken, tokenCanAdmin],*/ controlador.index);
