4303c0faf75be21ab774f521cd20469d2493843b
Marina Laguna Valdepeñas <123579383+demon-for-arcangel@users.noreply.github.com>
2024-05-07 19:08:19 +0200

    update server.js

--- back/database/events/eventsConnection.js
+++ back/database/events/eventsConnection.js
@@ -1,1 +1,39 @@
-//hacer
+require("dotenv").config();
+const { Sequelize, Op } = require("sequelize");
+const models = require("../../models");
+const Conexion = require("../connection.js");
+
+const conexion = new Conexion();
+
+class EventsModel {
+    constructor() {}
+
+    async indexEvents() {
+        try {
+            const events = await models.indexEvents.findAll();
+            return events;
+          }catch (error){
+            console.error('Error al mostrar la lista de eventos: ', error);
+            throw error;
+        }
+    }
+
+    async getEventById(id) {
+        try {
+          const event = await models.Events.findByPk(id);
+          if (!event) {
+            throw new Error('Evento no encontrado');
+          }
+          return event;
+        } catch (error){
+          console.error('Error al mostrar el evento: ', error);
+          throw error;
+        }
+    }
+
+    createEvent = async () => {}
+
+    updateEvent = async () => {}
+
+    deleteEvent = async () => {}
+}
