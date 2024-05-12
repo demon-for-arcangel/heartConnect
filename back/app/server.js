197b415267205a5e54c93877387e42330cd3c676
Marina Laguna Valdepeñas <marinalaguna2004@gmail.com>
2024-05-12 22:26:03 +0200

    create table chat with model and migration

--- back/app/server.js
+++ back/app/server.js
@@ -43,7 +43,6 @@ class Server {
   
     this.routes();
     this.sockets();
-  
   }
   
   async start() {
---
+++ back/migrations/20240512201615-create-chat.js
@@ -0,0 +1,31 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable(process.env.TABLE_CHAT, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER
+      },
+      message: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable(process.env.TABLE_CHAT);
+  }
+};
