992dc378463b166171a927332705c381e052a452
Marina Laguna Valdepeñas <marinalaguna2004@gmail.com>
2024-05-26 02:17:48 +0200

    implemented the creation of events from the interface

--- front/src/app/components/events/create-event/create-event.component.ts
+++ front/src/app/components/events/create-event/create-event.component.ts
@@ -1,12 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { EventService } from '../../../services/event.service';
 
 @Component({
   selector: 'app-create-event',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, InputSwitchModule, CommonModule],
   templateUrl: './create-event.component.html',
   styleUrl: './create-event.component.css'
 })
 export class CreateEventComponent {
+  eventForm!: FormGroup;
+  roles: any[] = [];
 
+  constructor(private fb: FormBuilder, private eventService: EventService){}
+
+  ngOnInit() {
+    this.eventForm = this.fb.group({
+      name: ['', Validators.required],
+      des: ['', Validators.required],
+      date: ['', [Validators.required]],
+      public: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.eventForm.valid) {
+      this.eventService.createNewEvent(this.eventForm.value).subscribe(
+        event => {
+          console.log('Evento creado:', event);
+        },
+        error => {
+          console.error('Error al crear el evento:', error);
+        }
+      );
+    }
+  }
 }
