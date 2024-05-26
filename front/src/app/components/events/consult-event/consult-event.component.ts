6ca913e007d3b48355d0ec5f674aeae82b0c5e8e
Marina Laguna Valdepeñas <marinalaguna2004@gmail.com>
2024-05-26 03:02:48 +0200

    implements function to consult information event

--- front/src/app/components/events/consult-event/consult-event.component.ts
+++ front/src/app/components/events/consult-event/consult-event.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { EventService } from '../../../services/event.service';
+import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
 
 @Component({
   selector: 'app-consult-event',
@@ -8,5 +10,24 @@ import { Component } from '@angular/core';
   styleUrl: './consult-event.component.css'
 })
 export class ConsultEventComponent {
+  @Input() eventId!: string;
+  event: any = {};
+
+  constructor(private eventService: EventService, public ref: DynamicDialogRef, public config: DynamicDialogConfig){}
+
+  ngOnInit() {
+    const eventId = this.config.data.eventId;
+    if (eventId) {
+      this.eventService.getEventById(eventId).subscribe(event => {
+        if (event) {
+          this.event = event;
+        } else {
+          console.error('No se encontró el evento con el ID proporcionado.');
+        }
+      }, error => {
+        console.error('Error al obtener los detalles del evento:', error);
+      });
+    }
+  }
 
 }
