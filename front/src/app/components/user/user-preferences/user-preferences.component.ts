03da2553f9ca375e46e90b03227e9258072950a4
Marina Laguna Valdepeñas <marinalaguna2004@gmail.com>
2024-05-27 02:35:49 +0200

    update

---
+++ front/src/app/components/user/user-preferences/user-preferences.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-preferences',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-preferences.component.html',
+  styleUrl: './user-preferences.component.css'
+})
+export class UserPreferencesComponent {
+
+}
