1f07892f3f0f583f1762c891fb7a9315a000550e
Marina Laguna Valdepeñas <marinalaguna2004@gmail.com>
2024-05-28 14:07:58 +0200

    update

--- front/src/app/components/user/my-profile/my-profile.component.ts
+++ front/src/app/components/user/my-profile/my-profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { MenuComponent } from '../../shared/menu/menu.component';
 import { AuthService } from '../../../services/auth.service';
 import { UserService } from '../../../services/user.service';
@@ -10,11 +10,13 @@ import { MessageService } from 'primeng/api';
 import { ListFriendsComponent } from '../list-friends/list-friends.component';
 import { ShowFriendsComponent } from '../show-friends/show-friends.component';
 import { ShowLikeUsersComponent } from '../show-like-users/show-like-users.component';
+import { Assets } from '../../../interfaces/assets';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-my-profile',
   standalone: true,
-  imports: [MenuComponent],
+  imports: [MenuComponent, CommonModule],
   templateUrl: './my-profile.component.html',
   styleUrl: './my-profile.component.css',
   providers: [DialogService]
@@ -22,9 +24,14 @@ import { ShowLikeUsersComponent } from '../show-like-users/show-like-users.compo
 export class MyProfileComponent implements OnInit {
   user: any = {};
   userProfileImageUrl: string = '';
+  images: { imageUrl: string }[] = []; // Tu array de imágenes
+  editingIndex: number | null = null;
+  previewImage: string = '';
+  editing: boolean = false;
+  index: number = 0;
 
   ref: DynamicDialogRef | undefined;
-
+  
   constructor(
     private authService: AuthService,
     private userService: UserService,
@@ -32,7 +39,7 @@ export class MyProfileComponent implements OnInit {
     public dialogService: DialogService,
     private messageService: MessageService
   ) {}
-
+  
   ngOnInit() {
     const token = localStorage.getItem('user');
     console.log('token del usuario', token);
@@ -59,46 +66,60 @@ export class MyProfileComponent implements OnInit {
       });
     }
   }
-
-  editProfile(): void {
-    this.ref = this.dialogService.open(EditProfileComponent, {
-      header: 'Editar Mi Perfil',
-      modal: true,
-      width: '60%',
-      breakpoints: {
-        '960px': '75vw',
-        '640px': '90vw'
-      },
-      styleClass: 'custom-modal',
-      data: { id: this.user.id }
-    });
+  
+    editProfile(): void {
+      this.ref = this.dialogService.open(EditProfileComponent, {
+        header: 'Editar Mi Perfil',
+        modal: true,
+        width: '60%',
+        breakpoints: {
+          '960px': '75vw',
+          '640px': '90vw'
+        },
+        styleClass: 'custom-modal',
+        data: { id: this.user.id }
+      });
+    }
+  
+    editImage(index: number): void {
+      this.editingIndex = index;
+      this.editing = true;
+      this.previewImage = this.images[index].imageUrl;
+    }
+  
+    deleteImage(index: number): void {
+      // Lógica para eliminar la imagen en el índice dado
+      this.images.splice(index, 1);
+    }
+  
+    addImage(): void {
+      // Lógica para añadir una nueva imagen
+    }
+  
+    saveImage(): void {
+      if (this.editingIndex !== null) {
+        this.images[this.editingIndex].imageUrl = this.previewImage;
+        this.cancelEditing();
+      }
+    }
+  
+    cancelEditing(): void {
+      this.editingIndex = null;
+      this.editing = false;
+      this.previewImage = '';
+    }
+  
+    saveAllImages(): void {
+      // Lógica para guardar todas las imágenes
+      console.log('Guardando todas las imágenes...');
+    }
+  
+    showFriends(): void {
+      // Lógica para mostrar amigos
+    }
+  
+    showLikeUsers(): void {
+      // Lógica para mostrar personas de interés
+    }
   }
-
-  showFriends(): void {
-    this.ref = this.dialogService.open(ShowFriendsComponent, {
-      header: 'Lista de Amigos',
-      modal: true,
-      width: '60%',
-      breakpoints: {
-        '960px': '75vw',
-        '640px': '90vw'
-      },
-      styleClass: 'custom-modal',
-      data: { id: this.user.id }
-    });
-  }
-
-  showLikeUsers(): void {
-    this.ref = this.dialogService.open(ShowLikeUsersComponent, {
-      header: 'Lista de Personas que me Interesan',
-      modal: true,
-      width: '60%',
-      breakpoints: {
-        '960px': '75vw',
-        '640px': '90vw'
-      },
-      styleClass: 'custom-modal',
-      data: { id: this.user.id }
-    });
-  }
-}
+  
