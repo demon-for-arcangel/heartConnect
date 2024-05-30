7de6947d60eebd69e3da1ed7f6ed7963160f7ceb
Marina Laguna Valdepeñas <marinalaguna2004@gmail.com>
2024-05-30 02:29:14 +0200

    update styles for menu

--- front/src/app/components/shared/menu/menu.component.ts
+++ front/src/app/components/shared/menu/menu.component.ts
@@ -9,5 +9,18 @@ import { RouterModule } from '@angular/router';
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
+  opcionesMenu = [
+    { nombre: 'Ver eventos disponibles', accion: 'verEventos' },
+    { nombre: 'Modo noche', accion: 'modoNoche' },
+    // Puedes agregar más opciones aquí
+  ];
 
+  ejecutarAccion(accion: string) {
+    // Implementa la lógica para cada opción del menú
+    if (accion === 'verEventos') {
+      // Lógica para ver eventos disponibles
+    } else if (accion === 'modoNoche') {
+      // Lógica para activar el modo noche
+    }
+  }
 }
