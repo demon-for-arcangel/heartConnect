660f9b4e2e1dc2af77dffd104ff6ea91d0f16286
Marina Laguna Valdepeñas <marinalaguna2004@gmail.com>
2024-06-06 15:03:21 +0200

    update

--- front/src/app/components/preferences/edit-preferences/edit-preferences.component.ts
+++ front/src/app/components/preferences/edit-preferences/edit-preferences.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from '../../shared/menu/menu.component';
 
 @Component({
   selector: 'app-edit-preferences',
   standalone: true,
-  imports: [],
+  imports: [MenuComponent],
   templateUrl: './edit-preferences.component.html',
   styleUrl: './edit-preferences.component.css'
 })
