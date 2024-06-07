aab24f5c1cb223aad894b3326fe5ffebe4aa50fb
Marina Laguna Valdepeñas <marinalaguna2004@gmail.com>
2024-06-07 14:06:52 +0200

    update

--- front/src/app/components/preferences/edit-preferences/edit-preferences.component.ts
+++ front/src/app/components/preferences/edit-preferences/edit-preferences.component.ts
@@ -1,13 +1,61 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from '../../shared/menu/menu.component';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { PreferencesService } from '../../../services/preferences.service';
+import { AuthService } from '../../../services/auth.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-preferences',
   standalone: true,
-  imports: [MenuComponent],
+  imports: [MenuComponent, CommonModule, FormsModule],
   templateUrl: './edit-preferences.component.html',
-  styleUrl: './edit-preferences.component.css'
+  styleUrl: './edit-preferences.component.css',
+  providers: [DialogService]
 })
 export class EditPreferencesComponent {
+  relationshipType: string = '';
+  sportsInterest: number = 0;
+  artisticInterest: number = 0;
+  politicalInterest: number = 0;
+  hasOrWantsChildren: boolean = false;
+  interestedIn: string = '';
 
+  user: any = {};
+
+  ref: DynamicDialogRef | undefined;
+
+  constructor(
+    private preferencesService: PreferencesService,
+    private authService: AuthService,
+    public dialogService: DialogService
+  ){}
+
+  ngOnInit(): void {
+    const token = localStorage.getItem('user');
+    if (token) {
+      this.authService.getUserByToken(token).subscribe(user => {
+        if (user && user.id) {
+          this.user = user;
+          console.log(this.user)
+  
+          this.preferencesService.getUserPreferences(this.user.id).subscribe(
+            (data) => {
+              console.log(data)
+              this.relationshipType = data.relationship_type;
+              this.sportsInterest = data.sports;
+              this.artisticInterest = data.artistic;
+              this.politicalInterest = data.politicians;
+              this.hasOrWantsChildren = data.hasOrWantsChildren;
+              this.interestedIn = data.interest;
+            },
+            (error) => {
+              console.error('Error al obtener las preferencias del usuario', error);
+            }
+          );
+        }
+      })
+    }
+  }
 }
--- front/src/app/components/preferences/show-preferences/show-preferences.component.ts
+++ front/src/app/components/preferences/show-preferences/show-preferences.component.ts
@@ -2,13 +2,16 @@ import { Component, Input } from '@angular/core';
 import { MenuComponent } from '../../shared/menu/menu.component';
 import { PreferencesService } from '../../../services/preferences.service';
 import { AuthService } from '../../../services/auth.service';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { EditPreferencesComponent } from '../edit-preferences/edit-preferences.component';
 
 @Component({
   selector: 'app-show-preferences',
   standalone: true,
   imports: [MenuComponent],
   templateUrl: './show-preferences.component.html',
-  styleUrl: './show-preferences.component.css'
+  styleUrl: './show-preferences.component.css',
+  providers: [DialogService]
 })
 export class ShowPreferencesComponent {
   relationshipType: string = '';
@@ -20,9 +23,12 @@ export class ShowPreferencesComponent {
 
   user: any = {};
 
+  ref: DynamicDialogRef | undefined;
+
   constructor(
     private preferencesService: PreferencesService,
-    private authService: AuthService
+    private authService: AuthService,
+    public dialogService: DialogService
   ) { }
 
   ngOnInit(): void {
@@ -51,5 +57,18 @@ export class ShowPreferencesComponent {
         }
       })
     }
+  }
+
+  editPreferences(): void{
+    this.ref = this.dialogService.open(EditPreferencesComponent, {
+      header: 'Editar Preferencias',
+      modal: true,
+      width: '60%',
+      breakpoints: {
+        '960px': '75vw',
+        '640px': '90vw'
+      },
+      styleClass: 'custom-modal'
+    })
   }
 }
